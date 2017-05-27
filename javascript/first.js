2853e1e466395d84c5cdf83560a0ebb2178a2b0c
AAKASH HANDA <AAKASH HANDA>
2017-05-27 06:01:28 +0530

    a

---
+++ javascript/first.js
@@ -0,0 +1,105 @@
+string
+number
+boolen
+null
+undefined
+
+var a= "pranit"
+var  a =10.344
+var a = true/false
+var a = "pranit"
+a
+"pranit"
+typeof(a)
+"string"
+
+var a = 10
+typeof(a)
+"number"
+
+var a = true
+typeof(a)
+"boolean"
+
+var pranit = "javascript"
+pranit
+"javascript"
+
+var a ="pranit"
+undefined
+var b = "javascript"
+undefined
+a+b
+"pranitjavascript"
+var b = 1
+undefined
+var a = 1
+undefined
+a+b
+2
+a-b
+0
+10+10+"10"
+"2010"
+"10"+10+"10"
+"101010"
+"10"+10+10
+"101010"
+
+
+document.getElementsByTagName("h2")[0].innerHTML="pranit"
+"pranit"
+
+var a = " hi i am learning javascript"
+undefined
+a
+" hi i am learning javascript"
+a.replace("javascript","python")
+" hi i am learning python"
+var a = "  hi i am learning javascript  "
+undefined
+a.trim()
+"hi i am learning javascript"
+a.repeat(2)
+"  hi i am learning javascript    hi i am learning javascript  "
+a.toUpperCase()
+"  HI I AM LEARNING JAVASCRIPT  "
+a.trim().toUpperCase()
+"HI I AM LEARNING JAVASCRIPT"
+a.trim().toLowerCase()
+"hi i am learning javascript"
+a.slice(1,10)
+" hi i am "
+a.length
+31
+a.substr(4,8)
+" i am le"
+var a = "pranit"
+undefined
+var b = "javascript"
+undefined
+a.concat(b)
+"pranitjavascript"
+
+
+100+2
+102
+10072
+10072
+100/72
+1.3888888888888888
+var a = 100
+undefined
+a/2
+50
+
+100+2
+102
+10072
+10072
+100/72
+1.3888888888888888
+var a = 100
+undefined
+a/2
+50
