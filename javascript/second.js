e67dfda019ec21054c1f1ddebaab7a90b9ad6e50
AAKASH HANDA <AAKASH HANDA>
2017-05-28 08:37:20 +0530

    A

---
+++ javascript/second.js
@@ -0,0 +1,69 @@
+var a = 5;
+if(a%2==0){
+	console.log("num is even"+a)
+}else if (a%3==0){
+	console.log("divide by "+a)
+}
+else{
+	console.log("num is odd"+a)
+}
+
+var name = "parnit";
+
+if(name=="parnit"){
+	console.log("allow acces")
+}else{
+	console.log("don't allow")
+}
+
+
+function isEven(a){
+	if(a%2==0){
+	console.log("num is even"+a)
+	}else if (a%3==0){
+		console.log("divide by "+a)
+	}
+	else{
+		console.log("num is odd"+a)
+	}
+}
+
+function allow(name){
+	if(name=="parnit"){
+	console.log("allow acces")
+}else{
+	console.log("don't allow")
+}
+}
+
+
+function add(a,b){
+	return a+b
+}
+
+///Array
+var a = [1,2,3,4,5,6]
+var a = ["dsf","df","fd","gf","gf"]
+var b = [1,2,"a","b",3,true,4,"c",5]
+var a = [1,2,3,4,5,6,7,8,9]
+
+var parnit =[1,2,3,[4,5,6,[7,8,9],10,11],12,13]
+var parnit =[1,2,3,[4,5,6,[7,8,9],10,11],12,13]
+undefined
+pranit[0]
+
+parnit[0]
+1
+parnit[3][0]
+4
+parnit[3][3][0]
+7
+parnit[3]
+(6) [4, 5, 6, Array(3), 10, 11]
+parnit[3][3]
+(3) [7, 8, 9]
+parnit[3][3][0]
+7
+parnit[3][4]
+10
+var parnit = new Array()
