cf54b13a8189b7036cfeabdeec427972289826dd
AAKASH HANDA <AAKASH HANDA>
2017-06-24 05:52:40 +0530

    a

---
+++ javascript/four.js
@@ -0,0 +1,21 @@
+for(i=0;i<=10;i++){
+	console.log(i)
+}
+
+
+var a =[1,2,3,4,5,6,7,8]
+
+for(i=0;i<=a.length;i++){
+	console.log(a[i])
+}
+
+
+var a = "pranit";
+for(y=0;y<=a.length;y++){
+	console.log(a[y])
+}
+
+var a = "pranit";
+for (i=a.length;i>=0;i--){
+console.log(a[i])
+}
