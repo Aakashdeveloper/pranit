8d353db4c3e65596a0bce3317a9c00e0bb73a9b2
AAKASH HANDA <AAKASH HANDA>
2017-08-14 20:02:08 +0530

    A

---
+++ javascript/this.js
@@ -0,0 +1,9 @@
+function person(name,age,weight){
+	this.name= name;
+	this.age = age;
+	this.weight = 40
+}
+
+var even = new person("even1",10,40);
+even.skin="fair"
+var susan = new person("susan1",20)
